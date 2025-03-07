2fd6e0cbe409d59e39adf1bcfd672b069bfe0a4f
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-03-07 05:45:11 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,14 @@
+  #include <iostream>
+  #include <cmath>
+  
+  using namespace std;
+  
+  int main() {
+      cout << "Enter a number: ";
+      double num;
+      cin >> num;
+      
+      cout << "The result of the operation is: " << pow(num, 3) << endl;
+      
+      return 0;
+  }
