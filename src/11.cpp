6e0e47ebb3a8151fab3e4dcf73f1e3a40bc1942c
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-03-17 07:27:49 +0000

    Add 11.cpp

---
+++ src/11.cpp
@@ -0,0 +1,7 @@
+// Your code here
+int main() {
+  int x = 5; // replace with a random integer between 1 and 10
+  double y = 3.4; // replace with a random real number between 1 and 10
+  cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+  return 0;
+}
