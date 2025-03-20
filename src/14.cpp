3404c22090f448a7b9ecc71e7952f72c750e8f56
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-03-20 23:52:56 +0000

    Add 14.cpp

---
+++ src/14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+    int result = x + y;
+    cout << "The sum of " << x << " and " << y << " is: " << result << endl;
+    return 0;
+}
