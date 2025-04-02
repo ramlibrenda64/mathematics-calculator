665723177553b9be7cc55ec7aec96dd1a34f6dbe
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-04-02 14:45:57 +0000

    Add 22.cpp

---
+++ src/22.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    float result = num1 + num2;
+    cout << num1 << " + " << num2 << " = " << result << endl;
+    return 0;
+}
