a12402c28f9dd5ebc901d0c84571a258c7b57531
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-04-11 11:29:37 +0000

    Add 29.cpp

---
+++ src/29.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+    int sum = x + y;
+    cout << "The sum of " << x << " and " << y << " is: " << sum << endl;
+    return 0;
+}
