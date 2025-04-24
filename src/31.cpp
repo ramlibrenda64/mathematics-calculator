7f4681df12cbf6b4ae0cb8cc29afebef0725acda
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-04-24 15:45:34 +0000

    Add 31.cpp

---
+++ src/31.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    result = num1 + num2;
+    cout << "The sum is: " << result << endl;
+
+    return 0;
+}
