eb5bb4edb301af5aee696d00530d5d3e68d4bb2d
ramlibrenda64 <ramlibrenda64@gmail.com>
2025-04-25 16:23:20 +0000

    Add 32.cpp

---
+++ src/32.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+    int z = x + y;
+    cout << "The sum of " << x << " and " << y << " is " << z << endl;
+    return 0;
+}
